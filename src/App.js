93a1dc678aa975b369cf5f7212c7c8f9dca917e1
Esteban <stc.eipv@outlook.com>
2019-02-05 21:56:36 -0300

    Fin tic-tac-toe, falta inmutable exercise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { array } from 'prop-types';
 
 class App extends Component {
 
@@ -27,23 +28,97 @@ class App extends Component {
   }
 }
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+
+// // constructor(props){
+// //   super(props);
+// //   this.state = {
+// //     value: null,
+// //   };
+// // }
+
+//   render() {
+//     return (
+//       // etapa1-<button className="square" onClick={function() { alert('click'); }}>
+//       //Funcion de flecha
+//       // etapa2-<button className="square" onClick={() => alert('click')}>
+//       //etapa 3-<button className="square" onClick={() => this.setState({value: 'X'})} >
+//       <button
+//       className="square"
+//       onClick={() => this.props.onClick()}
+//     >
+//         {/* {this.props.value} */}
+//         {/* {this.props.value} */}
+//         {this.state.value}
+//       </button>
+//     );
+//   }
+// }
+
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square (props){
+  return(
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+  </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    //squares[i] = 'X';
+    //
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    //
+    squares[i] = this.state.xIsNext ? 'X' : 'O';    
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    //Etapa1- return <Square value={i}/>;
+    //Etapa2-return <Square value={this.state.squares[i]}/>;
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}/>
   }
 
   render() {
-    const status = 'Next player: X';
+    //const status = 'Next player: X';
+    //const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    //
+    const winner = calculateWinner(this.state.squares);
+    //
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -85,3 +160,23 @@ class Game extends React.Component {
 }
 
 export default Game;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
